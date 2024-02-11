9cc06c777b261b0056f980df9f56d005ce589641
Katlego Thobye <katlegothobye@yahoo.com>
2024-02-11 18:09:21 +0200

    update app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
 
   return (
-    <h1 className="text-3xl text-red font-bold underline">
+    <h1 className="text-3xl font-bold underline text-red-500">
       Hello world!
     </h1>
   )
