78d0636f5f0f1d65f74011d947d204cdf90bc1f1
Katlego Thobye <katlegothobye@yahoo.com>
2024-02-12 13:30:54 +0200

    add signup api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from './routes/user.routes.js';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config()
 
@@ -15,9 +16,13 @@ mongoose
     })
 
 const app = express();
+app.use(express.json());
+
+
 const port = 3000
 app.listen(port, () => {
     console.log(`Server is running on port ${port}!!!`)
 });
 
 app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,6 +19,6 @@ const userSchema = new mongoose.Schema(
     },  {timestamps: true}
 )
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('user', userSchema);
 
 export default User; 
