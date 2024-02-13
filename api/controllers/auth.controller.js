03daae085f0dd7625f94b662529d38b079382f86
Katlego Thobye <katlegothobye@yahoo.com>
2024-02-13 09:58:24 +0200

    create signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 import errorHandler from '../utils/error.js'
+import jwt from 'jsonwebtoken'; 
 
 export const signup = async (req, res, next) => {
 
@@ -23,6 +24,37 @@ export const signup = async (req, res, next) => {
         await newUser.save();
         res.status(201).json({message:"Signup successful"});
     } catch(error) {
+        next(error)
+    }
+};
+
+export const signin = async(req, res, next) => {
+    const {username, password} = req.body;
+    
+    if (!username || !password || username === "" || password === "") {
+        next(errorHandler(400, "All fields required"));
+    }
+
+    try{
+        const validUser = await User.findOne({username})
+        if(!validUser){
+            return next(errorHandler(400, "Matching credenitials not found"))
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword)  {
+            return next(errorHandler(400, "Matching credenitials not found"))
+        }
+
+    const token = jwt.sign(
+        {id: validUser._id}, process.env.JWT_SECRET
+    );
+
+    const {password: pass, ...rest} = validUser._doc
+
+    res.status(200).cookie('access_token', token, {
+        httpOnly: 'true'}).json(rest)
+
+    }catch (error){
         next(error)
     }
 };
