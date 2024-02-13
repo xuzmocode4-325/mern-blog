b61e1cc703ee0cb9813ad4e8efac1a17c8eba2d2
Katlego Thobye <katlegothobye@yahoo.com>
2024-02-13 10:08:13 +0200

    complete sign in page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,104 @@
-import React from 'react'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
+import {React,  useState} from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const SignIn = () => {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading]  = useState(false);
+  const navigate = useNavigate();
+  
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]: e.target.value.trim()})
+  }
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.password) {
+      return setErrorMessage('Please ensure all fields are filled.')
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(formData)
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false)
+        return setErrorMessage(data.message);
+      }
+      setLoading(false)
+      if(res.ok){
+        navigate('/')
+      }
+    } catch (error) {
+       setErrorMessage(error.message)
+       setLoading(false);
+    }
+  }
+
   return (
-    <div>SignIn</div>
+    <div className='max-h-screen mt-20'>
+      <div className="flex p-5 max-w-3xl mx-auto
+        flex-col md:flex-row gap-5 md:items-center ">
+          {/* left */}
+          <div className='flex-1 md:mt-20'>
+            <Link to="/" className='font-bold dark:text-white text-4xl'>
+              Wellness 
+                <span className='px-2 py-1 bg-gradient-to-r
+                  from-yellow-200 via-red-300
+                  to-orange-300 rounded-md text-white'>
+                  Hub
+                </span>
+            </Link>  
+            <div className="text-sm mt-5">
+              <p>
+              Cillum amet qui incididunt laboris commodo officia reprehenderit ipsum adipisicing tempor veniam.
+              </p>
+            </div>
+          </div>  
+          {/* right */}
+          <div className='flex-1 md:mt-20'>
+            <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+              <div className="">
+                <Label value='Username'/>
+                <TextInput type='text' autoComplete='username'
+                placeholder='Username' id='username' onChange={handleChange}/>
+                <Label value='Password'/>
+                <TextInput type='password' autoComplete='current-password' 
+                  placeholder='Password' id='password' onChange={handleChange}/>
+              </div>
+              <Button gradientDuoTone='redToYellow' type='submit'>
+              {
+                  loading ? (
+                    <>
+                      <Spinner size='sm'/>
+                      <span className='pl-3'>Loading...</span>
+                    </>
+                  ) : 'Sign Up'
+                }
+              </Button>
+            </form>
+            <div className="flex gap-2 text-sm mt-5">
+              <span>Don't have an account?</span>
+              <Link to='/sign-up' className='text-blue-600' disabled={loading}>
+                Sign Up
+              </Link>
+            </div>
+            {
+              errorMessage && (
+                <Alert className='mt-5 text-center font-semibold' color='failure'>
+                  {errorMessage}
+                </Alert>
+              )
+            }
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default SignIn;
+export default SignIn
