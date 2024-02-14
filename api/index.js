f6b857cdd84ab3b7e0ad0f15e4e2aaf67998bcf3
Katlego Thobye <katlegothobye@yahoo.com>
2024-02-14 20:12:40 +0200

    implement update route in api for user credentials

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 
 dotenv.config()
 
@@ -16,7 +17,9 @@ mongoose
     })
 
 const app = express();
+
 app.use(express.json());
+app.use(cookieParser())
 
 const port = 3000
 app.listen(port, () => {
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { test } from '../controllers/user.controller.js'
+import { test, update } from '../controllers/user.controller.js'
+import { authenticate } from '../utils/authenticate.js';
 
 const router = express.Router()
 
 router.get("/test", test);
+router.put("/update/:userId", authenticate, update);
 
 export default router;
